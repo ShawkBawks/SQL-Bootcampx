dc72c2459be3f6b8fe34222338ae2d5bf92d45ee
Logan Roberts <lroberts77@gmail.com>
2019-10-15 21:13:16 +0000

    Set up the queries for teachers and assistance requests

---
+++ 4_queries/1_total_teacher_assistance_requests.sql
@@ -0,0 +1,12 @@
+-- SELECT  COUNT(assistance_requests) as total_assistances, teachers.name as name
+-- FROM teachers
+-- JOIN assistance_requests ON teachers.id = teacher_id
+-- WHERE name = 'Waylon Boehm'
+-- GROUP BY name;
+
+
+SELECT count(assistance_requests.*) as total_assistances, teachers.name
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+WHERE name = 'Waylon Boehm'
+GROUP BY teachers.name;
